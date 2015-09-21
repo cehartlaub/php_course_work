81bc67036ff818256b7f6bbb057eb8dc4ee4aba3
cehartlaub <cehartlaub@madisoncollege.edu>
2015-09-21 22:46:09 +0000

    Debug "removeemail.php"

--- lab_3/sendemail.php
+++ lab_3/sendemail.php
@@ -14,8 +14,10 @@
           $text = $_POST['elvismail'];
           
           $query = mysqli_query "SELECT * FROM email_list";
-          $result = mysqli_query($dbc, $query);
-          $row = mysqli_fetch_array($result);
+          $result = mysqli_query($dbc, $query)
+          or die('Error querying database');
+          
+        
           while($row = mysqli_fetch_array($result)) {
               $first_name = $row['first_name'];
               $last_name = $row['last_name'];
