0dd1b74d4496e57c442a04377217e0b3a8c76806
cehartlaub <cehartlaub@madisoncollege.edu>
2015-10-03 19:18:58 +0000

    declare form variables

--- project_1/libs.php
+++ project_1/libs.php
@@ -9,17 +9,36 @@
         <p>Directions:</p> 
         <p>Enter fields below and submit for a funny short story</p>
         
-        <form action: method"post">
-            <label for="noun">Noun:</label>
-            <input type="text" id="noun" name="noun" /><br />
-            <label for="verb">Verb:</label>
-            <input type="text" id="verb" name="verb" /><br />
-            <label for="adjective">Adjective:</label>
-            <input type="text" id="adjective" name="adjective" /><br />
-            <label for="adverb">Adverb:</label>
-            <input type="text" id="adverb" name="adverb" /><br />
-            <input type="submit" value="submit" name="submit" />
-        </form>
+        <?php 
+            if(isset($_POST['submit'])){
+               $noun = $_POST['noun'];
+               $verb = $_POST['verb'];
+               $adjective = $_POST['adjective'];
+               $adverb = $_POST['adverb'];
+               $output_form = false;
+            }
+            
+            
+            
+             
+            if($output_form){ 
+        ?>
+              <form action: method"post">
+                 <label for="noun">Noun:</label>
+                 <input type="text" id="noun" name="noun" /><br />
+                 <label for="verb">Verb:</label>
+                 <input type="text" id="verb" name="verb" /><br />
+                 <label for="adjective">Adjective:</label>
+                 <input type="text" id="adjective" name="adjective" /><br />
+                 <label for="adverb">Adverb:</label>
+                 <input type="text" id="adverb" name="adverb" /><br />
+                 <input type="submit" value="submit" name="submit" />
+              </form>
+        <?php
+            }
+        ?>
+       
+             
         
         
         
