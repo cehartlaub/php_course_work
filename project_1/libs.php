d539adc5b837b9960bf97fc2cf40599fa1efff27
cehartlaub <cehartlaub@madisoncollege.edu>
2015-10-03 18:58:45 +0000

    add form

--- project_1/libs.php
+++ project_1/libs.php
@@ -5,10 +5,21 @@
     </head>
     
     <body>
-        <h3>Crazylibs!</h3> <br />
-        <p>Directions:</p> <br />
+        <h3>Crazylibs!</h3> 
+        <p>Directions:</p> 
         <p>Enter fields below and submit for a funny short story</p>
         
+        <form action: method"post">
+            <label for="noun">Noun:</label>
+            <input type="text" id="noun" name="noun" /><br />
+            <label for="verb">Verb:</label>
+            <input type="text" id="verb" name="verb" /><br />
+            <label for="adjective">Adjective:</label>
+            <input type="text" id="adjective" name="adjective" /><br />
+            <label for="adverb">Adverb:</label>
+            <input type="text" id="adverb" name="adverb" /><br />
+            <input type="submit" value="submit" name="submit" />
+        </form>
         
         
         
