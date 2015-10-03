6ec4bfdfed3536010c0eb43149508c1378d81baa
cehartlaub <cehartlaub@madisoncollege.edu>
2015-10-03 18:48:30 +0000

    add title + description

---
+++ project_1/libs.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+       <title>Madlibs</title>
+       <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    
+    <body>
+        <h3>Crazylibs!</h3> <br />
+        <p>Directions:</p> <br />
+        <p>Enter fields below and submit for a funny short story</p>
+        
+        
+        
+        
+    </body>
+</html>
