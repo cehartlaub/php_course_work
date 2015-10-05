c4041ccaddba24fb77c726a8338bc7ebd0855e1f
cehartlaub <cehartlaub@madisoncollege.edu>
2015-10-05 21:39:58 +0000

    add file validation/errors to addscore.php

--- lab_5/addscore.php
+++ lab_5/addscore.php
@@ -11,8 +11,9 @@
 
 <?php
 
-  require_once __DIR__ . 'appwars.php';
-  require_once __DIR__ . 'connectwars.php';
+  
+  define ('GW_UPLOADPATH', 'images/');
+  define ('GW_MAXFILESIZE', 32768);
   
   if (isset($_POST['submit'])) {
     // Grab the score data from the POST
@@ -21,34 +22,49 @@
     $screenshot = $_FILES['screenshot']['name'];
     
   
-  if (!empty($name) && !empty($score) && !empty($screenshot)) {
-    $target = GW_UPLOADPATH . $screenshot;
-    if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {  
-      // Connect to the database
-      $dbc = mysqli_connect('DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME');
+    if (!empty($name) && !empty($score) && !empty($screenshot)) {
+      if (($screenshot_type == 'image/gif') || ($screenshot_type == 'image/jpeg') ||
+        ($screenshot_type == 'image/pjpeg') || ($screenshot_type == 'image/png') ||
+        ($screenshot_size > 0) && ($screenshot_size <= GW_MAXFILESIZE)) {
+        if ($_FILES['screenshot']['error'] == 0) {
+          $target = GW_UPLOADPATH . $screenshot; 
+          if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {  
+            // Connect to the database
+            $dbc = mysqli_connect('localhost', 'cehartlaub', '123456', 'gwdb'); 
 
-      // Write the data to the database
-      $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score', '$screenshot')";
-      mysqli_query($dbc, $query);
+            // Write the data to the database
+            $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score', '$screenshot')";
+            mysqli_query($dbc, $query);
+      
+             // Confirm success with the user
+             echo '<p>Thanks for adding your new high score!</p>';
+             echo '<p><strong>Name:</strong> ' . $name . '<br />';
+             echo '<strong>Score:</strong> ' . $score . '</p>';
+             echo '<img src="' . GW_UPLOADPATH . $screenshot . '" alt="Score image" /></p>';
+             echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
 
-      // Confirm success with the user
-      echo '<p>Thanks for adding your new high score!</p>';
-      echo '<p><strong>Name:</strong> ' . $name . '<br />';
-      echo '<strong>Score:</strong> ' . $score . '</p>';
-      echo '<img src="' . GW_UPLOADPATH . $screenshot . '" alt="Score image" /></p>';
-      echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
+            // Clear the score data to clear the form
+            $name = "";
+            $score = "";
 
-      // Clear the score data to clear the form
-      $name = "";
-      $score = "";
-
-      mysqli_close($dbc);
+            mysqli_close($dbc);
+        }
+        else { 
+        echo '<p class="error">Sorry, there was a problem uploading your screen shot image.</p>';
+        }
+      }
     }
-  }
     else {
-      echo '<p class="error">Please enter all of the information to add your high score.</p>';
+      echo '<p class="error">The screen shot must be a GIF, JPEG, or PNG image file no ' .
+        'greater than ' . (GW_MAXFILESIZE / 1024) . ' KB in size.</p>';
     }
-  }
+     @unlink($_FILES['screenshot']['tmp_name']);
+    }
+      else {
+        echo '<p class="error">Please enter all of the information to add your high score.</p>';
+      }
+    }
+        
 ?>
 
   <hr />
